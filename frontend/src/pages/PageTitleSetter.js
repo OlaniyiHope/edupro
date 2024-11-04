1bd3becef36bee5082df53190bde1194027ad3a7
olaniyihope2 <olaniyihope2@gmail.com>
2024-11-04 13:00:33 +0100

    all

--- frontend/src/pages/PageTitleSetter.js
+++ frontend/src/pages/PageTitleSetter.js
@@ -2,33 +2,40 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { useLocation } from "react-router-dom";
 
-const pageTitleMap = {
-  "/": "Edu Pro Solution - No 1 School Management System in Nigeria",
-  "/innovation": "Innovating Education with Artificial Intelligence",
-  "/about": "About Edu Pro Solution",
-  // Add other routes with titles as necessary
+const pageMetadata = {
+  "/": {
+    title: "Edu Pro Solution - No 1 School Management System in Nigeria",
+    description:
+      "Edu Pro Solution offers a comprehensive school management system.",
+    image: "URL_TO_YOUR_IMAGE", // Replace with the actual image URL
+  },
+  "/innovation": {
+    title: "Innovating Education with Artificial Intelligence",
+    description: "Learn how AI is transforming education.",
+    image: "URL_TO_YOUR_IMAGE",
+  },
+  "/about": {
+    title: "About Edu Pro Solution",
+    description: "Discover the mission and vision of Edu Pro Solution.",
+    image: "URL_TO_YOUR_IMAGE",
+  },
+  // Add other routes with metadata as necessary
 };
 
 const PageTitleSetter = () => {
   const location = useLocation();
-  const pageTitle =
-    pageTitleMap[location.pathname] ||
-    "Edu Pro Solution - No 1 School Management System in Nigeria";
+  const { title, description, image } =
+    pageMetadata[location.pathname] || pageMetadata["/"];
 
   return (
     <Helmet>
-      <title>{pageTitle}</title>
-      <meta
-        name="description"
-        content="Edu Pro Solution offers a comprehensive school management system with features like online exams, report cards, school fees payment, and more."
-      />
-      <meta property="og:title" content={pageTitle} />
-      <meta
-        property="og:description"
-        content="Edu Pro Solution offers a comprehensive school management system with features like online exams, report cards, school fees payment, and more."
-      />
-      <meta property="og:image" content="URL to your image" />
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
       <meta property="og:url" content={window.location.href} />
+      <meta property="og:type" content="website" />
     </Helmet>
   );
 };
